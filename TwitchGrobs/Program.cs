826a95b6f338f6bff695da49272604d158e15aa0
pyroch <pyrodd@gmail.com>
2021-02-05 22:14:38 +0300

    suppressing bluetooth error logging and stuff

--- TwitchGrobs/Program.cs
+++ TwitchGrobs/Program.cs
@@ -33,10 +33,12 @@ namespace TwitchGrobs
         {
             foreach (var process in System.Diagnostics.Process.GetProcessesByName("chrome"))
                 process.Kill();
-
             var options = new ChromeOptions();
-
             options.AddArgument("user-data-dir=C:\\Users\\" + Environment.UserName + "\\AppData\\Local\\Google\\Chrome\\User Data");
+            options.AddArgument("--log-level=3");
+            //ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            //service.SuppressInitialDiagnosticInformation = true;
+
             using (IWebDriver driver = new ChromeDriver(options))
             {
                 WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
@@ -44,7 +46,6 @@ namespace TwitchGrobs
 
                 StreamerCheck(driver);
 
-                
                 while (true)
                 {
                     if (currentStreamer < currentOnline)
@@ -53,17 +54,16 @@ namespace TwitchGrobs
                         System.Threading.Thread.Sleep(5000);
                         try
                         {
-                        var button = driver.FindElement(By.XPath("/html/body/div[1]/div/div[2]/nav/div/div[3]/div[6]/div/div/div/div/button"));
-                        button.Click();
-                        System.Threading.Thread.Sleep(1000);
-                        
+                            driver.FindElement(By.XPath("/html/body/div[1]/div/div[2]/nav/div/div[3]/div[6]/div/div/div/div/button")).Click(); // Clicking on profile button to get % of drop
+                            //button.Click();
+                            System.Threading.Thread.Sleep(1000);
                             var percent = driver.FindElement(By.XPath("/html/body/div[5]/div/div/div/div/div/div/div/div/div/div/div/div[3]/div/div/div[1]/div[9]/a/div/div[2]/p[2]")).GetAttribute("textContent");
-                            Console.WriteLine(percent.GetUntilOrEmpty());
+                            Console.WriteLine("Percentage of drop: " + percent.GetUntilOrEmpty());
 
                             System.Threading.Thread.Sleep(900000); // Timer between checks streamers that live and how much percent is done.
-                            //System.Threading.Thread.Sleep(10000);
                             if (percent == "100")
                             {
+                                Console.WriteLine("100% on one of drops. Claiming and switching streamer.");
                                 ClaimDrop(driver);
                                 currentStreamer++;
                             }
@@ -74,17 +74,16 @@ namespace TwitchGrobs
                         }
                         catch
                         {
+                            Console.WriteLine("Couldnt find the button or something wrong. Switching streamer and waiting 1 minute.");
                             currentStreamer++;
-                            System.Threading.Thread.Sleep(10000);
+                            System.Threading.Thread.Sleep(60000);
                         }
-                        
                     }
                     else
                     {
                         currentStreamer = 0;
                     }
                 }
-               
             }
         }
 
@@ -106,24 +105,23 @@ namespace TwitchGrobs
                 for (int y = 2; y <= 4; y++)
                 {
                     string streamerHeader = $"/html/body/div[1]/div[2]/div[{y}]/a[{x}]/div[1]";
+                    var streamerName = driver.FindElement(By.XPath(streamerHeader)).FindElement(By.ClassName("drop-item__header-streamer")).FindElement(By.ClassName("username"));
+
+                    //a = a.FindElement(By.ClassName("drop-item__header-streamer"));
+                    //a = a.FindElement(By.ClassName("username"));
+
                     string statusHeader = $"/html/body/div[1]/div[2]/div[{y}]/a[{x}]/div[1]";
-                    var a = driver.FindElement(By.XPath(streamerHeader));
-                    a = a.FindElement(By.ClassName("drop-item__header-streamer"));
-                    a = a.FindElement(By.ClassName("username"));
-
                     var status = driver.FindElement(By.XPath(statusHeader)).FindElement(By.ClassName("drop-item__header-status")).GetAttribute("textContent");
-
                     status = string.Join("", status.Split(default(string[]), StringSplitOptions.RemoveEmptyEntries));
 
                     if (status == "Live")
                     {
-                        onlineList.Add(a.GetAttribute("textContent"));
+                        onlineList.Add(streamerName.GetAttribute("textContent"));
                         currentOnline++;
                     }
-
-                    //Console.WriteLine(a.GetAttribute("textContent") + status.GetAttribute("textContent"));
                 }
             }
+            Console.Clear();
             foreach (var a in onlineList)
                 Console.WriteLine(a + " is live.");
         }
