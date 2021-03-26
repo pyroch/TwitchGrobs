712737d7edcdbb4958aa939fd686b831b1489fdb
pyroch <pyrodd@gmail.com>
2021-03-26 12:12:45 +0300

    0.5.9

--- TwitchGrobs/Program.cs
+++ TwitchGrobs/Program.cs
@@ -27,7 +27,7 @@ namespace TwitchGrobs
 
     class Program
     {
-        const string title = "TwitchGrobs-0.5.8";
+        const string title = "TwitchGrobs-0.5.9";
 
         static List<string> onlineList = new List<string>();
         static List<string> alreadyWatched = new List<string>();
@@ -35,7 +35,7 @@ namespace TwitchGrobs
         static IWebDriver driver;
 
         //xpaths to elements
-        const string livePath = "/html/body/div[1]/div/div[2]/div/main/div[2]/div[3]/div/div/div[1]/div[1]/div[2]/div/div[1]/div/div/div/div[1]/a/div/div/button/div[2]/div/div/div/div/p";
+        const string livePath = "/html/body/div[1]/div/div[2]/div/main/div[2]/div[3]/div/div/div[1]/div[1]/div[2]/div/div[1]/div/div/div/div[1]/div/div/a/div[2]/div/div/div/div/p";
         const string offPath = "/html/body/div[1]/div/div[2]/div/main/div[2]/div[3]/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div/div[2]/div[1]/div[1]/div/div[1]/div/p";
         const string profileButton = "/html/body/div[1]/div/div[2]/nav/div/div[3]/div[6]/div/div/div/div/button";
         const string dropProgress = "/html/body/div[5]/div/div/div/div/div/div/div/div/div/div/div/div[3]/div/div/div[1]/div[9]/a/div/div[2]/p[2]";
